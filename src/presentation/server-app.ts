d83509a69c900dda9b54110a452a55b82fff532a
Luis Enrique Garcia Guevara <luisegg1101@gmail.com>
2024-01-24 18:57:35 -0500

    SaveFile use case

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,4 +1,5 @@
 import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 
 interface RunOptions {
     base: number;
@@ -7,12 +8,13 @@ interface RunOptions {
 }
 
 export class ServerApp {
-    static run({base, limit, showTable}: RunOptions) {
+    static run({ base, limit, showTable }: RunOptions) {
         console.log('Server running...');
-        const table = new CreateTable().execute({base, limit});
+        const table = new CreateTable().execute({ base, limit });
+        const wasCreated = new SaveFile().execute({ fileContent: table, fileName: `table-${base}` });
 
-        if(showTable) console.log(table);
+        if (showTable) console.log(table);
+        (wasCreated) ? console.log('File created true!') : console.log('File not created.');
     }
-
 
 }
